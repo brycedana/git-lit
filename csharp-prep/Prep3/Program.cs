cb8376a92be63d863d8d590ffaaf23e4e4cc95d7
Bryce Dana <brycewdana@gmail.com>
2023-09-16 13:51:39 -0600

    Magic!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int input = -1; 
+
+
+        while(input != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            input = int.Parse(Console.ReadLine());
+        //If statement to get the "higher" and "lower" outputs
+        if(magicNumber > input)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if(magicNumber < input)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Got it!");
+        }
+    }
     }
 }
