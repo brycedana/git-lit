873bcfb769683116a4722cce4e0e2eb05b717776
Bryce Dana <brycewdana@gmail.com>
2023-09-16 13:15:56 -0600

    For some reason, it will only output Hello Sandbox World

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string nombre = Console.ReadLine();
+    //Spanish because why not?
+        Console.WriteLine("What is your last name? ");
+        string apellido = Console.ReadLine();
+
+        Console.WriteLine($"Welcome to C# {nombre} {apellido}.");
     }
 }
