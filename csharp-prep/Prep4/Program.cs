a5d2c54179341536e21e683aaadd662407201187
Bryce Dana <brycewdana@gmail.com>
2023-09-16 14:03:20 -0600

    Max/Min

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new <int>();
+
+        int input = -1; 
+        while (input != 0)
+        {
+            Console.Write("Enter any number (0 to end)");
+
+            string output = Console.ReadLine();
+            input = int.Parse(output);
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        int sum = 0; 
+        foreach (int number in numbers)
+        {
+            sum += number; 
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        int max = numbers [0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                 max = number; 
+            }
+           
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
