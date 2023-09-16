522ee5085894492b33d8f067f9a9c1644b0c015a
Bryce Dana <brycewdana@gmail.com>
2023-09-16 13:33:14 -0600

    same case

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Input your grade percentage: ");
+        string Input = Console.ReadLine();
+        int percentage = int.Parse(Input);
+
+    //if-else if and else statements in regards to user input
+        if ((percentage >= 90|| percentage >= 80))
+        {
+            Console.WriteLine("You passed with flying colors!");
+        }
+        else if((percentage >= 70))
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Oof! Better luck next time! Keep working hard!");
+        }
     }
 }
