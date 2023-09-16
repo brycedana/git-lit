7e0a5b4df80a7d73489580d0df1e8bbd395c76ca
Bryce Dana <brycewdana@gmail.com>
2023-09-16 14:16:00 -0600

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string input = Console.ReadLine();
+
+        return input;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Input a number here: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number; 
+
+        return square; 
+    }
+    static void DisplayResult(string input, int square)
+    {
+        Console.WriteLine($"{input}, the square is {square}");
     }
 }
