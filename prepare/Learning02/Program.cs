ba10aa1e6ba53ff736921c7e348ca6ecccabb1a9
Bryce Dana <brycewdana@gmail.com>
2023-09-21 15:09:34 -0600

    Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job0 = new Job();
+        job0._jobtitle = "Software Engineer";
+        job0._company = "Microsoft";
+        job0._startyear = 2019;
+        job0._endyear = 2022;
+
+        Job job1 = new Job();
+        job1._jobtitle = "Manager";
+        job1._company = "Apple";
+        job1._startyear = 2022; 
+        job1._endyear = 2023; 
+
+        Resume myResume = new Resume();
+        myResume._name = "Bryce Dana";
+
+        myResume._jobs.Add(job0);
+        myResume._jobs.Add(job1);
+
+        myResume.Display();
+
+
     }
+}
+
+
+class Resume
+{
+
 }
