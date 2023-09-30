d47d9316326b1a12f68a20643dc7d40c0e710c1b
Bryce Dana <brycewdana@gmail.com>
2023-09-30 17:11:26 -0600

    Stuck

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,26 @@
+public class Scripture 
+{
+    public Scripture (string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+
+    }
+
+    private bool IsCompletelyHidden()
+    {
+
+    }
+
+}
+
+
+
